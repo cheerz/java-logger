50a6661d0548260e210dad0b37f04c9cd61fa44e
Cyril Leroux <cyril0leroux@gmail.com>
2019-09-18 16:08:19 +0200

    Removed call to Logger.initialize()

--- tracker/src/test/java/com/cyrillrx/tracker/StreamingTrackerTest.java
+++ tracker/src/test/java/com/cyrillrx/tracker/StreamingTrackerTest.java
@@ -24,8 +24,6 @@ public class StreamingTrackerTest {
 
     @BeforeClass
     public static void initLogger() {
-
-        Logger.initialize();
         Logger.addChild(new SystemOutLog(Severity.VERBOSE));
     }
 
